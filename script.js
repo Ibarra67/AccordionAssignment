6159760cc0d2cd882aff8044b2943f73965765dd
Ibarra67 <127575778+Ibarra67@users.noreply.github.com>
2023-06-02 16:38:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function accord1(){
+    let drop = document.getElementById('text1');
+    if (drop.style.display === "none") {
+        drop.style.display = "block";
+    } else {
+        drop.style.display = "none";
+    } 
+}
+
+function accord2(){
+    let drop = document.getElementById('text2');
+    if (drop.style.display === "none") {
+        drop.style.display = "block";
+    } else {
+        drop.style.display = "none";
+    } 
+}
+
+function accord3(){
+    let drop = document.getElementById('text3');
+    if (drop.style.display === "none") {
+        drop.style.display = "block";
+    } else {
+        drop.style.display = "none";
+    } 
+}
+
+function accord4(){
+    let drop = document.getElementById('text4');
+    if (drop.style.display === "none") {
+        drop.style.display = "block";
+    } else {
+        drop.style.display = "none";
+    } 
+}
